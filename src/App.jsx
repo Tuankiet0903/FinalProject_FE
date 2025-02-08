1341f6574f4ce1ae8ee96d6ad53c3c0bf4475ed1
VanTuDev <Vantu.dev@gmail.com>
2025-02-09 00:09:19 +0700

    update cac phan tu

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import BoardLayout from "./layouts/BoardLayout"; // Layout chứa Sidebar & Header
-import DashBoard from "./pages/DashBoards";
-import Home from "./pages/Home";
-import Inbox from "./pages/Inbox"; 
+import BoardLayout from "./layouts/BoardLayout"; // Layout chính
+import SettingLayout from "./layouts/SettingLayout"; // Layout cho Settings
+import Dashboard from "./pages/HomePage/DashBoards";
+import Home from "./pages/HomePage/Home";
+import Inbox from "./pages/HomePage/Inbox";
+import UserProfile from "./pages/Settings/UserProfile";
+import Settings from "./pages/Settings/Settings"; // Trang Settings
 
 export default function App() {
   return (
     <Router>
       <Routes>
+        {/* Layout chính */}
         <Route path="/" element={<BoardLayout />}>
-          <Route path="home" element={<Home/>} />
-          <Route path="inbox" element={<Inbox/>} />
-          <Route path="dashBoard" element={<DashBoard />} />
+          <Route path="home" element={<Home />} />
+          <Route path="inbox" element={<Inbox />} />
+          <Route path="dashboard" element={<Dashboard />} />
+        </Route>
+
+        {/* Layout riêng cho Settings */}
+        <Route path="/setting" element={<SettingLayout />}>
+          <Route index element={<Settings />} />
+          <Route path="profile" element={<UserProfile />} />
         </Route>
       </Routes>
     </Router>
--- src/pages/HomePage/DashBoards.jsx
+++ src/pages/HomePage/DashBoards.jsx
@@ -2,7 +2,8 @@ export default function Dashboard() {
   return (
     <div>
       <h1 className="text-2xl font-semibold text-black">Dashboard</h1>
-      <p>Trang Dashboard - Tín làm mấy cái bữa m làm vô trong này, nhớ chia components là được</p>
+      <p></p>
+      
     </div>
   );
 }

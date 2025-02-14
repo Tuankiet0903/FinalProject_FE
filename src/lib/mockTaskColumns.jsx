f403eee04f62ece714e0b131b682676401dfb375
TuanKiet091202 <tuankieta4hbt@gmail.com>
2025-02-14 13:28:24 +0700

    kanban v1

--- src/lib/mockTaskColumns.jsx
+++ src/lib/mockTaskColumns.jsx
@@ -1,25 +1,9 @@
-// const mockTaskColumns = [
-//   {
-//     columnId: 1,
-//     name: "To Do",
-//     color: "gray",
-//     status: 1,
-//     listId: 1,
-//   },
-//   {
-//     columnId: 2,
-//     name: "In Progress",
-//     color: "yellow",
-//     status: 2,
-//     listId: 2,
-//   },
-//   {
-//     columnId: 3,
-//     name: "Done",
-//     color: "green",
-//     status: 3,
-//     listId: 3,
-//   },
-// ];
+const mockTaskColumns = [
+   { columnId: 1, name: "To Do", color: "gray", status: 1, listId: 1 },
+   { columnId: 2, name: "Backlog", color: "blue", status: 2, listId: 2 },
+   { columnId: 3, name: "Process", color: "orange", status: 3, listId: 3 },
+   { columnId: 4, name: "Review", color: "purple", status: 4, listId: 4 },
+   { columnId: 5, name: "Done", color: "green", status: 5, listId: 5 },
+];
 
-// export default mockTaskColumns;
+export default mockTaskColumns;

a6d8154a9fcd968676ce5cc89b385995f23caab0
VanTuDev <Vantu.dev@gmail.com>
2025-03-03 21:06:24 +0700

    nottification

--- src/components/slideBar/SidebarHeader.jsx
+++ src/components/slideBar/SidebarHeader.jsx
@@ -35,6 +35,7 @@ export default function SidebarHeader({ setSelectedWorkspaceId }) {
         onClick={() => setIsOpen(!isOpen)}
         className="flex items-center gap-2 cursor-pointer"
       >
+        
         <div className="bg-red-500 px-3 py-2 text-white rounded">
           <span className="font-semibold">{currentWorkspace?.name?.charAt(0) || "W"}</span>
         </div>
